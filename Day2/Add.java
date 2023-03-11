45d20349179bacef25582681fca3c098346a7193
valpijhansi <127584908+valpijhansi@users.noreply.github.com>
2023-03-11 15:46:44 +0530

    Day2 programs done

---
+++ Day2/Add.java
@@ -0,0 +1,12 @@
+class Add {
+    public static void main(String arg[]) {
+        int a = 25;
+        int b = 25;
+        int c = a + b;
+        int d = a * b;
+        int k = d / c;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(k);
+    }
+}
---
+++ Day2/operators.java
@@ -0,0 +1,13 @@
+class operators {
+    public static void main(String args[])
+
+    {
+        int a = 25;
+        int b = 26;
+        if (a > b) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
