efba9af18d22b1278fe4cf31cda2743c3c69e601
valpijhansi <127584908+valpijhansi@users.noreply.github.com>
2023-04-09 16:54:50 +0530

    Day 39 programs done

---
+++ Day39/World/WildAnimals/Lion.java
@@ -0,0 +1,12 @@
+package World.WildAnimals;
+
+public class Lion {
+    public void LiveIn() {
+        System.out.println("It lives in forest");
+
+    }
+
+    public void Nature() {
+        System.out.println("Hunting the Animals");
+    }
+}
---
+++ Day39/World/WildAnimals/Tiger.java
@@ -0,0 +1,12 @@
+package World.WildAnimals;
+
+public class Tiger {
+    public void Special() {
+        System.out.println("It is state Wild Animal");
+
+    }
+
+    public void speed() {
+        System.out.println("It runs very fast");
+    }
+}
