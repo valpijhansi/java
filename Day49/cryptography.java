448456722ed86815cea8c000291a3a4c66c92c77
valpijhansi <127584908+valpijhansi@users.noreply.github.com>
2023-04-20 21:36:53 +0530

    Day 49 Assignment done

---
+++ Day49/cryptography.java
@@ -0,0 +1,24 @@
+class cryptography {
+
+    public static void main(String args[]) {
+        String str = "jgnnq yqtnf";
+        char a[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z' };
+        for (int i = 1; i <= 25; i++) {
+            for (int j = 0; j < str.length(); j++) {
+                if (!(str.charAt(j) == ' ')) {
+                    int index = str.charAt(j) - 97 - i;
+                    int curNow = index % 26;
+                    if (curNow < 0)
+                        curNow += 26;
+                    System.out.print(a[curNow]);
+                } else {
+                    System.out.print("");
+                }
+
+            }
+            System.out.println("\n");
+        }
+
+    }
+}
