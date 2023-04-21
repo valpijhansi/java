f2c0c841281ef7a52d83345bdd38080a12aa6ae0
valpijhansi <127584908+valpijhansi@users.noreply.github.com>
2023-04-21 21:44:00 +0530

    Brute force Algorithm Ex

---
+++ Day50/Decode.java
@@ -0,0 +1,29 @@
+class Decode {
+
+    static char A[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+    public static void main(String[] args) {
+        String cipher = "kbbov mvdlz";
+        decode(cipher);
+
+    }
+
+    static void decode(String str) {
+        String cipher = "";
+        for (int i = 1; i <= 25; i++) {
+            for (int j = 0; j < str.length(); j++) {
+                if (!(str.charAt(j) == ' ')) {
+                    int ind = str.charAt(j) - 97 - i;
+                    int cur = ind % 26;
+                    if (cur < 0)
+                        cur += 26;
+                    System.out.print(A[cur]);
+                } else {
+                    System.out.print("");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
