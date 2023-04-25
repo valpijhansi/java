a66820fd07aed33ce6623456020942ad77df3281
valpijhansi <127584908+valpijhansi@users.noreply.github.com>
2023-04-25 22:11:29 +0530

    Realation b/w clasees & cmdline Args

---
+++ Day54/CMDline.java
@@ -0,0 +1,6 @@
+class CMDline {
+    public static void main(String[]  xyz) {
+        int sum = Integer.parseInt(xyz[0]) + Integer.parseInt(xyz[1]);
+        System.out.print(sum);
+    }
+}
---
+++ Day54/fullName.java
@@ -0,0 +1,9 @@
+class fullName {
+    public static void main(String[] xyz) {
+        String FullName = "";
+        for (var x : xyz) {
+            FullName += x + " ";
+        }
+        System.out.println(FullName);
+    }
+}
