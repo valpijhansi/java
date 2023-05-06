615e0f364b662e3af28bdca37233ebda5ad48308
valpijhansi <127584908+valpijhansi@users.noreply.github.com>
2023-05-06 12:33:46 +0530

    identifiers nullpoint && inputmismatchExceptions

---
+++ Day64/Identifiers/src/InpuMismatch.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class InpuMismatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();//when we will give matched value then it will print the value of n othetrwise it shows an exception like input mismatchException.
+        System.out.println(n);
+    }
+}
