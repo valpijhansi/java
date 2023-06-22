c180881997ddc7563169a5ef8c1da9ad6bd6927e
Valpi Jhansi <127584908+valpijhansi@users.noreply.github.com>
2023-06-22 21:41:35 +0530

    concept of good code in java

---
+++ Day108/Bloaters.java
@@ -0,0 +1,14 @@
+public class Bloaters {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+         //Point p = new Point(5,7);
+         boolean isRaining = true;
+         if(isRaining) 
+         System.out.println("It is raining");
+         else
+         System.out.println("It is not raining");
+    }
+   
+}
