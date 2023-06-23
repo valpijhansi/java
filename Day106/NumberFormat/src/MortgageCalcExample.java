f275a41306dfe6038feb34797cf0156ff073b465
Valpi Jhansi <127584908+valpijhansi@users.noreply.github.com>
2023-06-23 23:19:48 +0530

    concept of mortgage Calc

--- Day106/NumberFormat/src/MortgageCalcExample.java
+++ Day106/NumberFormat/src/MortgageCalcExample.java
@@ -1,7 +1,7 @@
 import java.text.NumberFormat;
 import java.util.Scanner;
 
-public class MortgageCalcExample {
+ class MortgageCalcEx {
      static final byte MONTHS_IN_YEAR = 12;
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
