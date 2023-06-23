20515f8af55337fe7bd824a6a8a3371c5cd81a2f
Valpi Jhansi <127584908+valpijhansi@users.noreply.github.com>
2023-06-23 22:33:14 +0530

    Concept of garbage collection

--- Day109/mock/src/Main.java
+++ Day109/mock/src/Main.java
@@ -1,27 +1,86 @@
-class Main {
+import java.text.NumberFormat;
+import java.util.Scanner;
 
-        public static void main(String[] args) {
+public class Main {
 
-            System.out.println(Main1.multiply(12.5 , 13.6));
-            System.out.println(Main1.multiply(2,3,6));
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+    public static void main(String[] args) {
 
-        }
+        int principal = (int) readInput("Principal", 1000, 1000000000);
+
+        float annualRate = (float) readInput("Annual Interest Rate", 1, 48);
+        float monthlyRate =  getMonthlyRate(annualRate);
+
+        byte years = (byte) readInput("Time (In Years)", 1,30);
+        short noOfPayments =  getNumOfPayments(years);
+
+        double mortage = findMortage(principal, monthlyRate, noOfPayments);
+        String formatedMonthlyAmount =  formatAmount(mortage);
+        System.out.println("Monly Rate : "+formatedMonthlyAmount);
+
+        printPaymentSchedule(noOfPayments,principal,monthlyRate);
+        
+
     }
 
-    class Main1 {
-        static int multiply(int a, int b) {
-            return (a * b);
+    public static String formatAmount(double amount) {
+        
+        return NumberFormat.getCurrencyInstance().format(amount);
+    }
+
+    public static double readInput(String prompt,int min,int max) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double value;
+        while(true) {
+
+            System.out.println(prompt+": ");
+            value = sc.nextInt();
+            if(value >= min && value <= max ) {
+                break;
+            }
+            System.out.println("Enter a value between "+min+" and "+max);
 
         }
-
-        static double multiply(double c, double d) {
-            return (c * d);
-        }
-
-        static int multiply(int x, int y, int z) {
-            return (x * y * z);
-        }
-
+        return value;
 
     }
+
+    public static float getMonthlyRate(float annualRate) {
+
+        float monthlyRate =  annualRate/PERCENT/MONTHS_IN_YEAR;
+        return monthlyRate;
+
+    }
+
+    public static short getNumOfPayments(byte years) {
+
+        return (short) (years*MONTHS_IN_YEAR);
+    }
+    
+
+    public static double findMortage(int principal,float monthlyRate,short noOfPayments) {
+
+        double mortage = principal * (monthlyRate * Math.pow((1+monthlyRate),noOfPayments)) / ((Math.pow((1+monthlyRate),noOfPayments))-1);
+        return mortage;  
+  
+      }
+
+      public static void printPaymentSchedule(short noOfPayments,int principal,float monthlyRate) {
+
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+
+        for(short month =1;month<=noOfPayments;month++) {
+
+            double balance = principal*(Math.pow((1+monthlyRate),noOfPayments)-Math.pow((1+monthlyRate),month)) /
+                                         (Math.pow((1+monthlyRate),noOfPayments)-1);
+
+            String formattedAmount  = formatAmount(balance);
+            System.out.println(formattedAmount);                           
+        }
+      }
+}
 
