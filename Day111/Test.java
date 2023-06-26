07668f874aac4cf5bb37d0ed3ca9c78e57f9d1ef
Valpi Jhansi <127584908+valpijhansi@users.noreply.github.com>
2023-06-26 21:04:40 +0530

    Cloneble object Methods

---
+++ Day111/Test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Test {
+     public static void main(String args[]) throws Exception
+    {
+        Map m = new HashMap();
+        m = System.getProperties();
+        Random r = new Random();
+
+        while (true) {
+            m.put(r.nextInt(), "randomValue");
+        }
+    }
+}
