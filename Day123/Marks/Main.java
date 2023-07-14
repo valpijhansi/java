930b2caa9ca86487945c767e944ac7ac245d61d2
Valpi Jhansi <127584908+valpijhansi@users.noreply.github.com>
2023-07-14 23:55:00 +0530

    Assignment

--- Day123/Marks/Main.java
+++ Day123/Marks/Main.java
@@ -1,2 +1,14 @@
-package Employee;public class Main {
+package Marks;
+
+public class Main {
+    public static void main(String[] args) {
+        Marks m = new Marks();
+         m.in();
+        int totalMarks = m.Sum();
+        System.out.println("\nSum of marks: "+totalMarks);
+
+        double averageMarks = m.Avg();
+        System.out.println("\nAverage marks: "+averageMarks);
+
+    }
 }
--- Day123/Marks/Marks.java
+++ Day123/Marks/Marks.java
@@ -1,25 +1,93 @@
-package Employee;
+package Marks;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class employee {
-    String rollNo;
-    String name;
-    int marks[] = new int[3];
+public class  Marks {
+    int subject1;
+    int subject2;
+    int subject3;
 
-    public void input() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the rollNo: ");
-        rollNo = scan.next();
 
-        System.out.println("Enter the Name: ");
-        name = scan.next();
+    public Marks() {
+        subject1 = 0;
+        subject2 = 0;
+        subject3 = 0;
 
-        for (int i = 0; i <marks.length; i++) {
-                System.out.println("Enter the Marks of Subject: "+(i+1));
-                marks[i] = scan.nextInt();
-
-            }
-        }
     }
 
+    public void in() {
+        Scanner scan = new Scanner(System.in);
+
+
+            System.out.print("Enter the marks of subject 1 : ");
+            subject1 = scan.nextInt();
+
+            System.out.print("Enter the marks of  subject 2 : ");
+            subject2 = scan.nextInt();
+            System.out.print("Enter the marks of subject 3: ");
+            subject3 = scan.nextInt();
+            validMarks();
+            validMarks();
+
+
+
+    }
+
+    private void validMarks() {
+        if((subject1 >= 0 && subject1 <=100)&&(subject2 >= 0 && subject2 <=100)&&(subject3 >=0 && subject3 <= 100)){
+            System.out.println(subject1+","+subject2+","+subject3);
+
+        }
+        else {
+            throw new InputMismatchException("Enter valid marks");
+        }
+
+    }
+
+
+    public int Sum() {
+        int result = subject1 + subject2 + subject3;
+        return result;
+    }
+
+    public double Avg() {
+        double res = Sum() / 3;
+        return res;
+    }
+
+    private  String NumberFormat(double number) {
+
+        String res = String.valueOf(number);
+
+        int count = 0;
+
+        for (int i = 0; i < res.length(); i++) {
+
+            if (res.charAt(i) == '.') {
+
+                for (int j = i + 1; j < res.length(); j++) {
+
+                    char digit = res.charAt(j);
+
+                    if (digit != '0') {
+                        count++;
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        if (count == 0) {
+
+            return String.valueOf((int) number);
+
+        } else {
+
+            return String.valueOf(String.format("%.2f", number));
+
+        }
+
+    }
+}
