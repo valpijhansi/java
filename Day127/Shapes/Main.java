049180fde6bd4c0d256faee1901779f632f0fa3f
Valpi Jhansi <127584908+valpijhansi@users.noreply.github.com>
2023-07-24 21:49:28 +0530

    Assignment Done

---
+++ Day127/Shapes/Main.java
@@ -0,0 +1,10 @@
+package Shapes;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+         r.setBreadth(12);
+         r.setLength(13);
+        System.out.println(r.Area());
+    }
+}
---
+++ Day127/Shapes/Rectangle.java
@@ -0,0 +1,42 @@
+package Shapes;
+
+public class Rectangle {
+    private int length;
+    private int breadth;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+
+    public void setBreadth(int breadth) {
+        this.breadth = breadth;
+    }
+
+
+    Rectangle(){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    double Area(){
+        return length * breadth;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", breadth=" + breadth +
+                '}';
+    }
+
+
+}
